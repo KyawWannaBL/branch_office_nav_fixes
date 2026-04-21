a7170d4549fec8645e662ef6a1e51b26fb28cac2
KyawWannaBL <mgkyawwanna@gmail.com>
2026-04-21 22:34:45 +0630

    Switch marketing route from HashRouter to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-...
-<BrowserRouter>
-  <AppRoutes />
-</BrowserRouter>
 import {
   SidebarProvider,
   SidebarInset,
