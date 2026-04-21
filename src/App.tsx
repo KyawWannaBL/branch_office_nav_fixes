6551eaf20e10aa8b12ca68463a479d8b2d3d4cbe
KyawWannaBL <mgkyawwanna@gmail.com>
2026-04-21 14:56:20 +0630

    Fix Waybill import path case mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import CreateDelivery from "./pages/CreateDelivery";
 import WayManagement from "./pages/WayManagement";
 import Deliverymen from "./pages/Deliverymen";
 import Merchants from "./pages/Merchants";
-import Waybill from "@/pages/waybill";
+import Waybill from "@/pages/Waybill";
 import Reporting from "./pages/Reporting";
 import Settings from "./pages/Settings";
 import SupervisorPortal from "./pages/SupervisorPortal";
